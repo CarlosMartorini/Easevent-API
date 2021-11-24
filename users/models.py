733c75307c55bc6e2fb35d3b978a477f81630539
Lucira Silva <luciracs0@gmail.com>
2021-11-24 15:17:53 -0300

    feat: add solo, phone and hour_price fields to user model

--- users/models.py
+++ users/models.py
@@ -1,4 +1,8 @@
+from django.contrib.auth import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 class User(AbstractUser):
-    ...
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    solo = models.BooleanField(blank=True, null=True)
+    hour_price = models.FloatField(blank=True, null=True)
