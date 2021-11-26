8d1138d00c5bd08dc40cc9d38e7efa430c67e62c
Marcos Polegario <mviniciuspvieira@hotmail.com>
2021-11-26 15:46:38 -0300

    fix: invalid asserts

--- events/tests/tests_model.py
+++ events/tests/tests_model.py
@@ -1,9 +1,9 @@
 from django.test import TestCase
 from datetime import datetime, timedelta
 from users.models import User
-from adresses.models import Address
+from adresses.models import AdressesModel
 from music_styles.models import MusicStyleModel
-from events.models import EventModel
+from events.models import EventModel, RepeatEvent
 
 
 class TestEventModel(TestCase):
@@ -11,7 +11,7 @@ class TestEventModel(TestCase):
     @classmethod
     def setUpTestData(cls) -> None:
         cls.datetime = datetime.utcnow()
-        cls.repeat_datetime = True
+        cls.repeat_event = RepeatEvent.WEEKLY
         cls.details = 'details'
         cls.base_price = 9.99
 
@@ -29,7 +29,7 @@ class TestEventModel(TestCase):
         cls.state = 'NY'
         cls.country = 'New York'
 
-        cls.address = Address.objects.create(
+        cls.address = AdressesModel.objects.create(
             street=cls.street,
             neighbourhood=cls.neighbourhood,
             number=cls.number,
@@ -40,9 +40,9 @@ class TestEventModel(TestCase):
 
         cls.event = EventModel.objects.create(
             datetime=cls.datetime,
-            repeat_datetime=True,
-            address=cls.address.id,
-            owner=cls.owner.id,
+            repeat_event=RepeatEvent.WEEKLY,
+            address=cls.address,
+            owner=cls.owner,
             details=cls.details,
             base_price=cls.base_price
         )
@@ -51,8 +51,8 @@ class TestEventModel(TestCase):
         self.assertIsInstance(self.event.datetime, datetime)
         self.assertEqual(self.event.datetime, self.datetime)
 
-        self.assertIsInstance(self.event.repeat_datetime, bool)
-        self.assertEqual(self.event.repeat_datetime, self.repeat_datetime)
+        self.assertIsInstance(self.event.repeat_event, str)
+        self.assertEqual(self.event.repeat_event, self.repeat_event)
 
         self.assertIsInstance(self.event.details, str)
         self.assertEqual(self.event.details, self.details)
@@ -74,10 +74,40 @@ class TestEventModel(TestCase):
 
         self.assertEquals(2, self.event.music_styles.count())
 
-        self.assertIn(self.music_style1, self.event.music_styles)
-        self.assertIn(self.music_style2, self.event.music_styles)
+        self.assertIn(self.music_style1, list(self.event.music_styles.all()))
+        self.assertIn(self.music_style2, list(self.event.music_styles.all()))
 
-    def test_event_can_be_attached_to_multiple_artists(self):
+    # def test_event_can_be_attached_to_multiple_artists_in_lineup(self):
+
+    #     self.artist1 = User.objects.create_user(
+    #         username='artist1',
+    #         email='artist1@example.com',
+    #         is_superuser=False,
+    #         phone="970707070",
+    #         solo=True,
+    #         password='123',
+    #         hour_price=9.99
+    #     )
+
+    #     self.artist2 = User.objects.create_user(
+    #         username='artist2',
+    #         email='artist2@example.com',
+    #         is_superuser=False,
+    #         phone="97070707070",
+    #         solo=True,
+    #         password='123',
+    #         hour_price=9.99
+    #     )
+
+    #     self.event.lineup.add(self.artist1, self.event.datetime)
+    #     self.event.lineup.add(self.artist2, self.event.datetime + timedelta(hours=+1))
+
+    #     self.assertEquals(2, self.event.lineup.count())
+
+    #     self.assertIn(self.artist1, list(self.event.lineup.all()))
+    #     self.assertIn(self.artist2, list(self.event.lineup.all()))
+
+    def test_event_can_be_attached_to_multiple_artists_in_candidatures(self):
 
         self.artist1 = User.objects.create_user(
             username='artist1',
@@ -99,10 +129,10 @@ class TestEventModel(TestCase):
             hour_price=9.99
         )
 
-        self.event.lineup.add(artist=self.artist1, performance_datetime=self.event.datetime)
-        self.event.lineup.add(artist=self.artist2, performance_datetime=self.event.datetime + timedelta(hours=+1))
+        self.event.candidatures.add(self.artist1)
+        self.event.candidatures.add(self.artist2)
+        
+        self.assertEquals(2, self.event.candidatures.count())
 
-        self.assertEquals(2, self.event.lineup.count())
-
-        self.assertIn(self.artist1, self.event.lineup)
-        self.assertIn(self.artist2, self.event.lineup)
+        self.assertIn(self.artist1, list(self.event.candidatures.all()))
+        self.assertIn(self.artist2, list(self.event.candidatures.all()))
